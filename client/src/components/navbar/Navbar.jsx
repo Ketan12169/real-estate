ad1518bf737d1e1fce126e8ad5b4e2e129b5fff2
Ketan12169 <ketansingh890@gmail.com>
2024-07-06 12:42:26 +0530

    Initial commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from "react";
+import "./navbar.scss";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  if (currentUser) fetch();
+
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>Real Estate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/list"> List Page</a>
+      </div>
+      <div className="right">
+        {currentUser ? (
+          <div className="user">
+            <img
+              src={currentUser.avatar || "/noavatar.jpg"}
+              onClick={() => navigate("/profile")}
+              alt=""
+            />
+
+            <span>{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/login">Sign in</a>
+            <a href="/register" className="register">
+              Sign up
+            </a>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/list">ListPage</a>
+          <a href="/login">Sign in</a>
+          <a href="/register">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
